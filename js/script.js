da5c06edf08f3267cbbac6bd9e5249edb1cdab5f
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-07 16:56:20 +0200

    Ok ticket
    aggiunto ticket interarittivo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-
+// Variabili
+const priceKm = 0.21;
 var printTicket = document.getElementById("ticket");
 
-
+// Evento al click genera
 printTicket.addEventListener("click",
+// funzione
   function() {
+// Raccolgo le variabili dai campi del form
     var name = document.getElementById("form__input--name").value;
-    console.log(name);
     var km = document.getElementById("form__input--km").value;
-    console.log(km);
     var age = document.getElementById("form__input--age").value;
-    console.log(age);
+// Calcolo il coto del biglietto
+    var ticket = km * priceKm;
+// Valuto lo sconto in base all'età
+    if (age === "minorenne") {
+      ticket = ticket * 0.80;
+    }
+    else if (age >= "over65") {
+        ticket = ticket * 0.60;
+    }
+    else {
+        ticket = ticket;
+      }
+// Arrotondo il prezzo
+    ticket = (Math.round(ticket * 100) / 100);
+// Compilo la tabella con gli input
+    document.getElementById('nome-passeggero').innerHTML = name;
+    document.getElementById('offerta').innerHTML = age;
+    document.getElementById('costo').innerHTML = ticket
   }
 );
