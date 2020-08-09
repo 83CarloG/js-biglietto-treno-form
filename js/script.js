ce65b9838214aa364ee2d445b17c0a8dc4e47ed8
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-09 16:10:15 +0200

    js 100%

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 const priceKm = 0.21;
 var printTicket = document.getElementById("ticket");
 
+  function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 // Evento al click genera
 printTicket.addEventListener("click",
 // funzione
@@ -10,6 +15,12 @@ printTicket.addEventListener("click",
     var name = document.getElementById("form__input--name").value;
     var km = document.getElementById("form__input--km").value;
     var age = document.getElementById("form__input--age").value;
+// Random numbers
+    var carrozza = Math.floor(Math.random()*100 + 1);
+    document.getElementById("carrozza").innerHTML =   carrozza;
+    var codiceCp = getRandomInt(90000, 100000);
+    document.getElementById("codice-cp").innerHTML =   codiceCp;
+
 // Calcolo il coto del biglietto
     var ticket = km * priceKm;
 // Valuto lo sconto in base all'età
@@ -39,7 +50,7 @@ printTicket.addEventListener("click",
 var annulla = document.getElementById("reset");
 annulla.addEventListener("click",
   function() {
-    document.getElementById("ticket-box").classList.add("box-no");
+    document.getElementById("ticket-box").classList.add("box--no");
 
     // reset campi
     document.getElementById("form__input--name").value = "";
