07706980d047cf2b9f67744b0c179f0febe5888f
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-24 09:58:45 +0200

    little fix

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const priceKm = 0.21;
 var printTicket = document.getElementById("ticket");
 // Function MIX
 function getRandomInt(min, max) {
-min = Math.ceil(min);
-max = Math.floor(max);
-return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
 // Evento al click genera
@@ -21,9 +21,8 @@ printTicket.addEventListener("click",
     var carrozza = Math.floor(Math.random()*100 + 1);
     document.getElementById("carrozza").innerHTML =   carrozza;
     var codiceCp = getRandomInt(90000, 100000);
-    document.getElementById("codice-cp").innerHTML =   codiceCp;
-
-// Calcolo il coto del biglietto
+    document.getElementById("codice-cp").innerHTML =   codiceCp;s
+// Calcolo il costo del biglietto
     var ticket = km * priceKm;
 // Valuto lo sconto in base all'età
     if (age === "minorenne") {
@@ -40,7 +39,7 @@ printTicket.addEventListener("click",
 // Compilo la tabella con gli input
     document.getElementById('nome-passeggero').innerHTML = name;
     document.getElementById('offerta').innerHTML = "Sconto " + age;
-    document.getElementById('costo').innerHTML = ticket
+    document.getElementById('costo').innerHTML = ticket + "€"
 // Faccio apparire il box del biglietto
     var boxNo = document.getElementById("ticket-box");
     boxNo.classList.remove("box--no");
